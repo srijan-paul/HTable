78aa644f0b9159387165f66df9dea7edbd2d3827
srijan-paul <srijannr1@gmail.com>
2021-02-05 21:55:28 +0530

    robinhood hashing with linear probe

--- src/htable.hpp
+++ src/htable.hpp
@@ -2,6 +2,7 @@
 
 #include <cmath>
 #include <cstddef>
+#include <cstdint>
 #include <iterator>
 #include <stdint.h>
 #include <vector>
@@ -25,30 +26,45 @@ class HTable {
 		key_type key;
 		value_type value;
 		hash_type hash = -1; // hash value is cached
+		std::uint32_t probe_count = 0;
 
 		bool operator==(const HTEntry& other) const {
 			return hash == other.hash and key == other.key;
 		}
 	};
 
+	template <typename Th, typename Rt>
+	Rt& get(Th* pThis, const key_type& key, const hash_type hash) {
+		std::size_t mask = pThis->m_cap - 1;
+		std::size_t index = hash & mask;
+
+		while (true) {
+			HTEntry& entry = pThis->m_entries[index];
+			if (entry.key == key or entry.hash == -1) return entry;
+			index = (index + 1) & mask;
+		}
+		return pThis->m_entries[0];
+	}
+#define NTK_HT_GET_CONST(k, h) get<const HTable<Kty, Vty, Kty_Hasher>, const HTEntry>(this, k, h)
+
+#define NTK_HT_GET(k, h) get<HTable<Kty, Vty, Kty_Hasher>, HTEntry>(this, k, h)
+
 	HTEntry* m_entries = new HTEntry[DefaultCapacity];
 	std::size_t m_num_entries = 0;
 	std::size_t m_cap = DefaultCapacity;
-	std::uint32_t m_growth_pow2 = InitialCapLog2;
 
 	void ensure_capacity() {
 		if (m_num_entries < m_cap * LoadFactor) return;
 		std::size_t old_cap = m_cap;
 		m_cap *= GrowthFactor;
-		m_growth_pow2++;
 		HTEntry* old_entries = m_entries;
 		m_entries = new HTEntry[m_cap];
 
 		for (std::size_t i = 0; i < old_cap; ++i) {
 			HTEntry& entry = old_entries[i];
 			if (entry.hash == -1) continue;
-			HTEntry& new_entry = get(entry.key, entry.hash);
-			new_entry = entry;
+			HTEntry& new_entry = NTK_HT_GET(entry.key, entry.hash);
+			new_entry = std::move(entry);
 		}
 
 		delete[] old_entries;
@@ -63,53 +79,62 @@ class HTable {
 		return m_entries[0];
 	}
 
-	const HTEntry& find(const key_type& key) {
-		HTEntry& entry = get(key, m_hasher(key));
-		if (entry.hash == -1) return null();
-		return entry;
+	const HTEntry& find(const key_type& key) const {
+		return NTK_HT_GET_CONST(key, m_hasher(key));
 	}
 
-	const HTEntry& find_by_hash(const hash_type& hash, const key_type& key) {
-		return get(key, hash);
+	HTEntry& find(const key_type& key) {
+		return NTK_HT_GET(key, m_hasher(key));
 	}
 
-	bool set(const key_type& key, const value_type& value) {
+	bool set(key_type key, value_type value) {
 		ensure_capacity();
-		const hash_type& hash = m_hasher(key);
-		HTEntry& entry = get(key, hash);
-		if (entry == null()) return false;
-		if (entry.hash == -1) m_num_entries++;
-		entry.key = key;
-		entry.value = value;
-		entry.hash = hash;
+		hash_type hash = m_hasher(key);
+		std::size_t mask = m_cap - 1;
+
+		std::size_t index = hash & mask;
+		std::uint32_t dist = 0;
+
+		while (true) {
+			HTEntry& entry = m_entries[index];
+
+			if (entry.hash == -1) {
+				entry.key = std::move(key);
+				entry.value = std::move(value);
+				entry.hash = hash;
+				entry.probe_count = dist;
+				m_num_entries++;
+				return true;
+			}
+
+			if (entry.key == key) {
+				entry.value = std::move(value);
+				entry.probe_count = dist;
+			}
+
+			if (entry.probe_count < dist) {
+				std::swap(hash, entry.hash);
+				std::swap(key, entry.key);
+				std::swap(value, entry.value);
+				dist = entry.probe_count;
+			}
+
+			index = (index + 1) & mask;
+			dist++;
+		}
 		return true;
 	}
 
-	value_type& operator[](const key_type& key) {
-		const hash_type hash = m_hasher(key);
-		HTEntry& entry = get(key, hash);
-		return entry.value;
+	const value_type& operator[](const key_type& key) {
+		return NTK_HT_GET_CONST(key, m_hasher(key)).value;
 	}
 
 	~HTable() {
 		delete[] m_entries;
 	}
-
-  private:
-	HTEntry& get(const key_type& key, const hash_type& hash) {
-		std::size_t cap = m_cap - 1;
-		std::size_t first_idx = hash & cap;
-		std::size_t index = first_idx;
-		std::int32_t x = 1;
-		while (true) {
-			HTEntry& entry = m_entries[index];
-			if (entry.key == key or entry.hash == -1) return entry;
-			index = (first_idx + ((x * x + x) >> 1)) & cap;
-			x++;
-		}
-
-		return m_entries[0];
-	}
 };
+
+#undef NTK_HT_GET
+#undef NTK_HT_GET_CONST
 
 } // namespace ntk
