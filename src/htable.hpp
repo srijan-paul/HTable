5b9be16d42544be6c9cfe565366ad5fc55e180b7
srijan-paul <srijannr1@gmail.com>
2021-02-05 14:05:40 +0530

    probing is now quadratic

--- src/htable.hpp
+++ src/htable.hpp
@@ -1,16 +1,19 @@
 #pragma once
 
+#include <cmath>
 #include <cstddef>
 #include <iterator>
+#include <stdint.h>
 #include <vector>
 
 namespace ntk {
 
 template <typename Kty, typename Vty, class Kty_Hasher = std::hash<Kty>>
 class HTable {
-	static constexpr std::size_t DefaultCapacity = 16;
+	static constexpr std::size_t InitialCapLog2 = 8;
+	static constexpr std::size_t DefaultCapacity = pow(2, InitialCapLog2);
 	static constexpr std::uint8_t GrowthFactor = 2;
-	static constexpr float LoadFactor = 0.75;
+	static constexpr float LoadFactor = 0.5;
 
 	using hash_type = std::int64_t;
 	using key_type = Kty;
@@ -28,7 +31,28 @@ class HTable {
 		}
 	};
 
-	std::vector<HTEntry> m_entries{DefaultCapacity};
+	HTEntry* m_entries = new HTEntry[DefaultCapacity];
+	std::size_t m_num_entries = 0;
+	std::size_t m_cap = DefaultCapacity;
+	std::uint32_t m_growth_pow2 = InitialCapLog2;
+
+	void ensure_capacity() {
+		if (m_num_entries < m_cap * LoadFactor) return;
+		std::size_t old_cap = m_cap;
+		m_cap *= GrowthFactor;
+		m_growth_pow2++;
+		HTEntry* old_entries = m_entries;
+		m_entries = new HTEntry[m_cap];
+
+		for (std::size_t i = 0; i < old_cap; ++i) {
+			HTEntry& entry = old_entries[i];
+			if (entry.hash == -1) continue;
+			HTEntry& new_entry = get(entry.key, entry.hash);
+			new_entry = entry;
+		}
+
+		delete[] old_entries;
+	}
 
   public:
 	explicit HTable() : m_hasher{Kty_Hasher()} {
@@ -50,9 +74,11 @@ class HTable {
 	}
 
 	bool set(const key_type& key, const value_type& value) {
+		ensure_capacity();
 		const hash_type& hash = m_hasher(key);
 		HTEntry& entry = get(key, hash);
 		if (entry == null()) return false;
+		if (entry.hash == -1) m_num_entries++;
 		entry.key = key;
 		entry.value = value;
 		entry.hash = hash;
@@ -62,17 +88,24 @@ class HTable {
 	value_type& operator[](const key_type& key) {
 		const hash_type hash = m_hasher(key);
 		HTEntry& entry = get(key, hash);
-		entry.hash = hash;
-		entry.key = key;
 		return entry.value;
+	}
+
+	~HTable() {
+		delete[] m_entries;
 	}
 
   private:
 	HTEntry& get(const key_type& key, const hash_type& hash) {
-		const std::size_t cap = m_entries.capacity();
-		for (std::size_t index = hash % cap;; index = (index + 1) % cap) {
+		std::size_t cap = m_cap - 1;
+		std::size_t first_idx = hash & cap;
+		std::size_t index = first_idx;
+		std::int32_t x = 1;
+		while (true) {
 			HTEntry& entry = m_entries[index];
 			if (entry.key == key or entry.hash == -1) return entry;
+			index = (first_idx + ((x * x + x) >> 1)) & cap;
+			x++;
 		}
 
 		return m_entries[0];
--- test.cpp
+++ test.cpp
@@ -1,21 +1,82 @@
+#include "clock.hpp"
 #include "src/htable.hpp"
+#include <array>
 #include <cstdint>
+#include <fstream>
 #include <string>
+#include <unordered_map>
 
-struct SHasher {
-	std::int64_t operator()(const std::string& s) const {
+static const std::size_t nruns = 100;
+static std::vector<std::string> ks;
+static std::vector<std::string> vs;
+
+struct SimpleHasher {
+	const std::size_t operator()(const std::string& s) const {
 		return s.length();
 	}
 };
 
-int main() {
-	ntk::HTable<std::string, int> table;
+void read_from_file(const std::string& file_name, std::vector<std::string>& ks,
+					std::vector<std::string>& vs) {
+	std::ifstream file(file_name);
+	while (true) {
+		std::string* k = new std::string();
+		if (!std::getline(file, *k)) break;
+		std::string* v = new std::string();
+		if (!std::getline(file, *v)) break;
+		ks.emplace_back(*k);
+		vs.emplace_back(*v);
+	}
+}
 
-	printf("%d\n", table.set("heeheee", 123));
-	printf("%d\n", table.set("foofoo", 789));
-	printf("%d\n", table.set("aaabbb", 456));
-	printf("%d\n", table["foofoo"]);
-	printf("%d\n", table["heeheee"]);
-	printf("%d\n", table["aaabbb"]);
+void prepare_bench_suite() {
+	static const char* filename = "benchmark/dictionary.txt";
+	read_from_file(filename, ks, vs);
+}
+
+void benchmark_unp() {
+	Clock clock{"std::unordered_map", nruns};
+
+	for (std::size_t i = 0; i < nruns; ++i) {
+		std::unordered_map<std::string, std::string> table;
+		for (std::size_t i = 0; i < ks.size(); ++i) {
+			table[ks[i]] = vs[i];
+		}
+
+		for (std::size_t i = 0; i < ks.size(); ++i) {
+			if (table.find(ks[i])->second != vs[i]) {
+				exit(0);
+			}
+		}
+	}
+}
+
+void benchmark_table() {
+	Clock clock{"ntk::HTable", nruns};
+
+	for (std::size_t i = 0; i < nruns; ++i) {
+		ntk::HTable<std::string, std::string> table;
+		for (std::size_t i = 0; i < ks.size(); ++i) {
+			table.set(ks[i], vs[i]);
+		}
+
+		for (std::size_t i = 0; i < ks.size(); ++i) {
+			if (table.find(ks[i]).value != vs[i]) {
+				exit(0);
+			}
+		}
+	}
+}
+
+void benchmark() {
+	prepare_bench_suite();
+	benchmark_unp();
+	benchmark_table();
+	std::cout << "\n\n";
+}
+
+int main() {
+	ntk::HTable<std::string, std::size_t> table;
+	benchmark();
 	return 0;
 }
